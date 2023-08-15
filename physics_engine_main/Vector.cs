a6d795e33b2c09a7eee9cf783f0ca2e335a2e1c7
Tim <85653488+Ecume@users.noreply.github.com>
2023-08-15 03:56:52 -0400

    added a vector class and overloaded the adittion operator to make the vector math faster

---
+++ physics_engine_main/Vector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace physics_engine_main
+
+{
+    public readonly struct Vector
+    {
+        public readonly float X;
+        public readonly float Y;
+
+        public Vector(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X + b.X, a.Y + b.Y);
+        }
+    } 
+}
